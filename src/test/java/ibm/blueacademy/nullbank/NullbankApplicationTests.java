ef8065e9fdf93e48e50a227fa413c1a55cb99f1b
Marivaldo Sena <marivaldo.sena@live.com>
2021-10-30 14:16:55 -0300

    Initial commit.

---
+++ src/test/java/ibm/blueacademy/nullbank/NullbankApplicationTests.java
@@ -0,0 +1,13 @@
+package ibm.blueacademy.nullbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
